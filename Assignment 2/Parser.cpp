0cb12b5bebe9eecaa9cf894cf2906d8409cf405a
Fady Hanna <fadyhanna@aucegypt.edu>
2019-05-14 17:28:43 +0200

    Basic functions

--- Assignment 2/Parser.cpp
+++ Assignment 2/Parser.cpp
@@ -50,3 +50,151 @@ void Parser::program() // program = declaration_list
     // TODO: Call declaration_list() which is grammer rule no. 2
 }
 
+void Parser::type_specifier(){
+    // int | float
+    if(next_token.substr(0, 3) == "int")
+        match("int");
+    else if (next_token.substr(0, 5) == "float")
+        match("float");
+}
+void Parser::relop(){
+    // <= | < | > | >= | == | !=
+    if(next_token.substr(0, 2) == "<=")
+        match("<=");
+    else if (next_token.substr(0, 1) == "<")
+        match("<");
+    else if (next_token.substr(0, 1) == ">")
+        match(">");
+    else if (next_token.substr(0, 2) == ">=")
+        match(">=");
+    else if (next_token.substr(0, 2) == "==")
+        match("==");
+    else if (next_token.substr(0, 2) == "!=")
+        match("!=");
+    
+}
+void Parser::addop(){
+    // addop        = + | -
+    if (next_token.substr(0, 1) == "+")
+        match("+");
+    else if (next_token.substr(0, 1) == "-")
+        match("-");
+}
+void Parser::mulop(){
+    // mulop        = * | /
+    if (next_token.substr(0, 1) == "*")
+    match("*");
+    else if (next_token.substr(0, 1) == "/")
+    match("/");
+}
+
+void Parser::factor(){
+    //factor        = ( expression ) | ID | FLOAT | INT
+    if (next_token.substr(0, 1) == "("){
+        match("(");
+        expression();
+        match(")");
+    }
+    else if (next_token.substr(0, 2) == "ID")
+        match("ID");
+    else if (next_token.substr(0, 5) == "FLOAT")
+        match("FLOAT");
+    else if (next_token.substr(0, 3) == "INT")
+    match("INT");
+
+}
+
+void Parser::write_var(){
+    //write-var    = expression | STRING
+    if (next_token.substr(0, 6) == "STRING")
+        match("STRING");
+    else
+        expression();
+}
+void Parser::write_stmt(){
+    //write-stmt    = write write-var {, write-var};
+    if (next_token.substr(0, 5) == "write"){
+        match("write");
+        write_var();
+        while(next_token.substr(0, 1) == ","){
+            match(",");
+            write_var();
+        }
+        match(";");
+    }
+}
+
+void Parser::read_stmt(){
+    //read-stmt    = read ID {, ID};
+    if (next_token.substr(0, 5) == "read"){
+        match("ID");
+        while(next_token.substr(0, 1) == ","){
+            match(",");
+            match("ID");
+        }
+        match(";");
+    }
+}
+
+void Parser::term(){
+    //term        = factor { mulop factor }
+    factor();
+    while(next_token.substr(0, 1) == "*" || next_token.substr(0, 1) == "/"){
+        mulop();
+        factor();
+    }
+}
+
+void Parser::additive_expression(){
+    //additive-expression = term { addop term }
+    term();
+    while(next_token.substr(0, 1) == "+" || next_token.substr(0, 1) == "-"){
+        addop();
+        term();
+    }
+}
+void Parser::expression(){
+    //expression = additive-expression [ relop additive-expression ]
+    //Will search for first terminal that expression can lead to, like those
+    //                      ( expression ) | ID | FLOAT | INT
+    if(next_token.substr(0,1) == "(" ||
+       next_token.substr(0,2) == "ID" ||
+       next_token.substr(0,5) == "FLOAT" ||
+       next_token.substr(0,3) == "INT"){
+        additive_expression();
+        //again                 relop = <= | < | > | >= | == | !=
+        if(next_token.substr(0,2) == "<=" ||
+           next_token.substr(0,1) == "<" ||
+           next_token.substr(0,1) == ">" ||
+           next_token.substr(0,2) == ">="||
+           next_token.substr(0,2) == "<="||
+           next_token.substr(0,2) == "=="||
+           next_token.substr(0,2) == "!="){
+            relop();
+            additive_expression();
+        }
+    }
+}
+void Parser::iteration_stmt(){
+    //iteration-stmt = while ( expression ) stmt
+    if(next_token.substr(0,5) == "while"){
+        match("while");
+        match("(");
+        expression();
+        match(")");
+        stmt();
+    }
+}
+void Parser::selection_stmt(){
+    //selection-stmt = if ( expression ) stmt [ else stmt ]
+    if(next_token.substr(0,2) == "if"){
+        match("if");
+        match("(");
+        expression();
+        match(")");
+        stmt();
+        if(next_token.substr(0,4) == "else"){
+        }
+        
+    }
+}
--- Assignment 2/Parser.h
+++ Assignment 2/Parser.h
@@ -6,10 +6,15 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+
 #include <stdio.h>
 #include <fstream>
 #include <stdlib.h>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
 using namespace std;
 
 class Parser
@@ -23,11 +28,34 @@ class Parser
 
     void program(); // grammer rule no. 1
     
+  
+    void declaration_list();
+    void var_declaration();
+    void type_specifier();
+    void compound_stmt();
+    void stmt();
+    void expression_stmt();
+    void selection_stmt();
+    void iteration_stmt();
+    void expression();
+    void relop();
+    void additive_expression();
+   void  addop();
+    void term();
+    void mulop();
+    void factor();
+    void write_stmt();
+    void write_var();
+    void read_stmt();
+    
+    
     // TODO: Add the rest of the rules and private variables you will need to use within the class.
 
   public:
     string output_filepath;
     void run_program(string);
+    
+    string next_token;
 };
 
 #endif /* PARSER_H */
