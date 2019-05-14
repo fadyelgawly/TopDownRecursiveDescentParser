f4517b0b6c4fd6d4787901754bbe9c5aa5089235
Fady Hanna <fadyhanna@aucegypt.edu>
2019-05-14 15:57:49 +0200

    Initial Commit

---
+++ Assignment 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 2
+//
+//  Created by Fady on 5/14/19.
+//  Copyright © 2019 Fady Hanna. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
